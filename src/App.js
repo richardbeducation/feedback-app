e93bc13e5d88b970eb828a70ffb12ca5930a150d
richardbeducation <richardbeducation@gmail.com>
2022-05-19 08:13:12 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 // our main app component
-
 function App() {
-    return <h1>Hello from the App component</h1>
+  const title = 'Blog Post'
+  const body = 'This is my blog post'
+  const comments = [
+    { id: 1, text: 'Comment one' },
+    { id: 2, text: 'Comment two' },
+    { id: 3, text: 'Comment three' },
+  ]
+
+  return (
+    <div className="container">
+      <h1>{title}</h1>
+      <p>{body}</p>
+
+      <div className="comments">
+        <h3>Comments ({comments.length})</h3>
+        <ul>
+          {comments.map((comment, index) => (
+            <li key={index}>{comment.text}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default App
