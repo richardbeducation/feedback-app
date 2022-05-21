83e32169d0d4963c666ac0ac463fdae5f7977160
richardbeducation <richardbeducation@gmail.com>
2022-05-21 08:29:30 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 // our main app component
 function App() {
-  const title = 'Blog Post'
-  const body = 'This is my blog post'
-  const comments = [
-    { id: 1, text: 'Comment one' },
-    { id: 2, text: 'Comment two' },
-    { id: 3, text: 'Comment three' },
-  ]
-
   return (
     <div className="container">
-      <h1>{title}</h1>
-      <p>{body}</p>
-
-      <div className="comments">
-        <h3>Comments ({comments.length})</h3>
-        <ul>
-          {comments.map((comment, index) => (
-            <li key={index}>{comment.text}</li>
-          ))}
-        </ul>
-      </div>
+      <h1>My App</h1>
     </div>
   )
 }
